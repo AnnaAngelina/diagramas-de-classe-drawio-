846f412189a4e103b90db3dac35c407ecbb90d4c
Angelina <annaangeh169@gmail.com>
2024-11-17 16:05:18 -0300

    definições de get e set

--- relacionamentos.py/relacionamentos_encaps.py
+++ relacionamentos.py/relacionamentos_encaps.py
@@ -10,10 +10,9 @@ c1 = Cliente()
 c1.__nome = 'Anna'
 c1.__endereco = 'Major Sales'
 c1.__cpf = '119.347.294-34'
-c1.__idade = 16
 print(c1.__cpf)
 c1.mudaCPF('166.920.814-16')
 print(c1.__cpf)
 #encapsulamento: proteger atributos de acessoas externos, os métodos serão públicos mas os atributos serão privados pois precisamos acessar.
-# get e set
-# pega a informação para acessar sem ser diretamente (sempre retorna algo), salva e enviar algo.
+# get e set(usados no encapsulamento para manusear atributos privados uma vez que eles não devem ser acessados diretamente por segurança)
+# o get pega a informação para acessar, sempre retorna algo, ou seja, lê e armazena o dado do objeto. Enquanto o set altera ou atribui valores a membros de dados.
