f1b0b98021d890a62e0cc0e4dc925338731f6255
Anna Angelina <154215704+AnnaAngelina@users.noreply.github.com>
2024-11-19 11:49:39 -0300

    Create cliente_class.py

---
+++ cliente_class.py
@@ -0,0 +1,10 @@
+from pessoa import Pessoa
+
+class Cliente(Pessoa):
+    def __init__(self, nomep, cpfp, enderecop, limitecomprap):
+        Pessoa.__init__(nomep, cpfp, enderecop)
+        self.__limitecompra = limitecomprap
+    def getLimite(self):
+        print('Limite: ', self.__limitecompra)
+    def falar(self):
+        print('Olá! Gostaria de realizar uma compra.')
