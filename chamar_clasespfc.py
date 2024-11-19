ac943c477d861d540f27e993291668944b461376
Anna Angelina <154215704+AnnaAngelina@users.noreply.github.com>
2024-11-19 11:56:43 -0300

    Create chamar_clasespfc.py

---
+++ chamar_clasespfc.py
@@ -0,0 +1,11 @@
+from pessoa import Pessoa
+p1 = Pessoa('Ciro', 123, 'Rua X')
+p1.falar()
+
+from funcionario import Funcionario
+f1 = Funcionario('Ciro', 123, 'Rua X', 'Vendedor', 400)
+f1.falar()
+
+from cliente import Cliente
+c1 = Cliente('Ciro', 123, 'Rua X', 200)
+c1.falar()
