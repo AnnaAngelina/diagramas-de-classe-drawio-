1f2681525d9ec753c52457c090c9acef9134e51c
Anna Angelina <154215704+AnnaAngelina@users.noreply.github.com>
2024-11-19 11:46:12 -0300

    Create funcionario_class.py

---
+++ funcionario_class.py
@@ -0,0 +1,11 @@
+from pessoa import Pessoa
+
+class Funcionario(Pessoa):
+    def __init__(self, nomep, cpfp, enderecop, cargop, salariop):
+        Pessoa.__init__(self, nomep, cpfp, enderecop)
+        self.__cargo = cargop
+        self.__salario = salariop
+    def getCargo(self):
+        print('Cargo:'+self.__cargo)
+    def falar(self):
+        print('Olá! Como posso ajudar?')
